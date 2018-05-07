61da3a5f3f2b7ea8e1614a779731796afcc8fb43
Elías Augusto Tuyú Alamilla <elias.tuyu@springlabs.net>
2018-05-07 08:29:46 -0500

    Improve DirectChatPage component

--- development/react/pages/DirectChatPage.js
+++ development/react/pages/DirectChatPage.js
@@ -1,74 +1,64 @@
 import React from 'react';
 import axios from 'axios';
-
 import DirectChat from '../controls/DirectChat';
+import Page from '../controls/Page';
+import PageBody from '../controls/PageBody';
 
-class DirectChatPage extends React.Component
-{
-	constructor(props)
-	{
+class DirectChatPage extends React.Component {
+	constructor(props) {
 		super(props);
-		this.state =
-		{
-			chatData: [],
+		this.state = {
+			chatData: []
 		}
 		this.processData = this.processData.bind(this);
 	}
 
-	componentDidMount()
-	{
+	componentDidMount() {
 		axios.get('static/chats.txt')
-		.then(result => {
-			this.setState({
-				chatData: result.data
-			})
-			this.processData();
-		})
-		.catch(error => {
-			this.setState({
-				chatData: []
-			})
-		});
+			.then(result => {
+				this.setState({
+					chatData: result.data
+				});
+				this.processData();
+			}).catch(error => {
+				this.setState({
+					chatData: []
+				})
+			});
 	}
 
-    render()
-    {
-        return(
-            <section className="content">
-				<DirectChat title="Chat directo" data={this.state.chatData} height='500px' disabled></DirectChat>
-            </section>
-        );
-    }
+  render() {
+    return(
+      <Page>
+				<PageBody>
+					<DirectChat
+						data={this.state.chatData}
+						disabled
+						height='500px'
+						title="Chat directo"/>
+				</PageBody>
+      </Page>
+    );
+  }
 
-	processData()
-	{
+	processData() {
 		var data = [{}];
 		var count = 0;
 		this.state.chatData.map(
 			c => {
-				// console.log(c);
-				if(c.name != data[count].name)
-				{
+				if (c.name != data[count].name) {
 					count += 1;
 					data[count] = c;
-
-					data[count].message.map(
-						x => x.date = data[count].date
-					);
+					data[count].message.map(x => x.date = data[count].date);
 					delete data[count].date;
-				}
-				else
-				{
-					c.message.map(
-						x => {
-							var obj = {};
-							obj.type = x.type;
-							obj[obj.type] = x[x.type];
-							obj.date = c.date;
-							data[count].message.push(obj);
-						}
-					);
-
+				} else {
+					c.message.map(x => {
+						var obj = {};
+						obj.type = x.type;
+						obj[obj.type] = x[x.type];
+						obj.date = c.date;
+						data[count].message.push(obj);
+					});
 				}
 			}
 		);
