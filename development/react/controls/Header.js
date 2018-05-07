f2d526e8a3cd74e0a28b724b1fbf8d29edfb4a32
Elías Augusto Tuyú Alamilla <elias.tuyu@springlabs.net>
2018-05-06 23:26:51 -0500

    Improve Header control

--- development/react/controls/Header.js
+++ development/react/controls/Header.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Header extends React.Component
-{
-    render()
-    {
-        return(
-            <header className="main-header">
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
 
-                <Link to="/" className="logo">
-                    {/* <span className="logo-mini"><b>A</b>LT</span> */}
-                    <span className="logo-lg">{this.props.title}</span>
-                </Link>
+  handleToggle(e) {
+    e.preventDefault();
+    this.props.onToggle();
+  }
 
-                <nav className="navbar navbar-static-top" role="navigation">
-                    {/* <a href="#" className="sidebar-toggle" data-toggle="push-menu" role="button" /> */}
-                    <div className="navbar-custom-menu">
-						{this.props.children}
+  render() {
+    return(
+      <header className="main-header">
+        <Link className="logo" to="/">
+          <span className="logo-lg">{ this.props.title }</span>
+        </Link>
+        <nav className="navbar navbar-static-top" role="navigation">
+          <a className="sidebar-toggle"
+            href="#"
+            onClick={ this.handleToggle }/>
+          <div className="navbar-custom-menu">
+						{ this.props.children }
 					</div>
-                </nav>
-            </header>
-        );
-    }
+        </nav>
+      </header>
+    );
+  }
 }
+
+Header.defaultProps = {
+  onToggle: function() {}
+};
 
 module.exports = Header;
