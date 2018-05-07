6313f971d06666d70a393b957a85306ddf04b251
Elías Augusto Tuyú Alamilla <elias.tuyu@springlabs.net>
2018-05-07 00:29:40 -0500

    Improve InfoBox control

--- development/react/controls/InfoBox.js
+++ development/react/controls/InfoBox.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
-class InfoBox extends React.Component
-{
-    render()
-    {
-        var cnColor = `info-box-icon bg-${this.props.color}`;
-        return(
-            <div className="info-box">
-                <span className={cnColor}><i className={this.props.icon}></i></span>
-                <div className="info-box-content">
-                    <span className="info-box-text">{this.props.title}</span>
-                    <span className="info-box-number">{this.props.children}</span>
-                </div>
-            </div>
-        );
-    }
+class InfoBox extends React.Component {
+  render() {
+    return(
+      <div className="info-box">
+        <span className={ `info-box-icon bg-${ this.props.color }` }>
+          <i className={ this.props.icon }></i>
+        </span>
+        <div className="info-box-content">
+          <span className="info-box-text">{ this.props.label }</span>
+          <span className="info-box-number">{ this.props.value }</span>
+        </div>
+      </div>
+    );
+  }
 }
+
+InfoBox.defaultProps = {
+  color: 'blue',
+  icon: 'fa fa-star-o'
+};
 
 module.exports = InfoBox;
